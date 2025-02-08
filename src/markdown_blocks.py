3b9c281399fe0aa72c28b7a4d6f27430b0bf55c3
Allanswer-A <admin@all-answer.com>
2025-02-08 11:35:52 +0100

    works now with a bit of magic and help from another git

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -21,38 +21,6 @@ def markdown_to_blocks(markdown):
     return filtered_blocks
 
 
-def block_to_block_type(block):
-    lines = block.split("\n")
-
-    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
-        return block_type_heading
-    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
-        return block_type_code
-    if block.startswith(">"):
-        for line in lines:
-            if not line.startswith(">"):
-                return block_type_paragraph
-        return block_type_quote
-    if block.startswith("* "):
-        for line in lines:
-            if not line.startswith("* "):
-                return block_type_paragraph
-        return block_type_ulist
-    if block.startswith("- "):
-        for line in lines:
-            if not line.startswith("- "):
-                return block_type_paragraph
-        return block_type_ulist
-    if block.startswith("1. "):
-        i = 1
-        for line in lines:
-            if not line.startswith(f"{i}. "):
-                return block_type_paragraph
-            i += 1
-        return block_type_olist
-    return block_type_paragraph
-
-
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     children = []
@@ -76,7 +44,46 @@ def block_to_html_node(block):
         return ulist_to_html_node(block)
     if block_type == block_type_quote:
         return quote_to_html_node(block)
-    raise ValueError("invalid block type")
+    raise ValueError("Invalid block type")
+
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_ulist
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_ulist
+        return block_type_ulist
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_olist
+    return block_type_paragraph
 
 
 def text_to_children(text):
@@ -103,15 +110,15 @@ def heading_to_html_node(block):
         else:
             break
     if level + 1 >= len(block):
-        raise ValueError(f"invalid heading level: {level}")
-    text = block[level + 1 :]
+        raise ValueError(f"Invalid heading level: {level}")
+    text = block[level + 1:]
     children = text_to_children(text)
     return ParentNode(f"h{level}", children)
 
 
 def code_to_html_node(block):
     if not block.startswith("```") or not block.endswith("```"):
-        raise ValueError("invalid code block")
+        raise ValueError("Invalid code block")
     text = block[4:-3]
     children = text_to_children(text)
     code = ParentNode("code", children)
@@ -143,7 +150,7 @@ def quote_to_html_node(block):
     new_lines = []
     for line in lines:
         if not line.startswith(">"):
-            raise ValueError("invalid quote block")
+            raise ValueError("Invalid quote block")
         new_lines.append(line.lstrip(">").strip())
     content = " ".join(new_lines)
     children = text_to_children(content)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import LeafNode, ParentNode, HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,40 +15,6 @@ class TestHTMLNode(unittest.TestCase):
             ' class="greeting" href="https://boot.dev"',
         )
 
-    def test_values(self):
-        node = HTMLNode(
-            "div",
-            "I wish I could read",
-        )
-        self.assertEqual(
-            node.tag,
-            "div",
-        )
-        self.assertEqual(
-            node.value,
-            "I wish I could read",
-        )
-        self.assertEqual(
-            node.children,
-            None,
-        )
-        self.assertEqual(
-            node.props,
-            None,
-        )
-
-    def test_repr(self):
-        node = HTMLNode(
-            "p",
-            "What a strange world",
-            None,
-            {"class": "primary"},
-        )
-        self.assertEqual(
-            node.__repr__(),
-            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
-        )
-
     def test_to_html_no_children(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -60,7 +26,8 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_with_children(self):
         child_node = LeafNode("span", "child")
         parent_node = ParentNode("div", [child_node])
-        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+        self.assertEqual(parent_node.to_html(),
+                         "<div><span>child</span></div>")
 
     def test_to_html_with_grandchildren(self):
         grandchild_node = LeafNode("b", "grandchild")
